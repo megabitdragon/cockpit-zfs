13c7f8a34b9b59ef431bef4f66f87ebe417a3196
JordanKeo45 <jkeough@45drives.com>
2023-07-05 15:15:23 -0300

    updates poolDetails modal

--- zfs/src/components/pool/PoolDetail.vue
+++ zfs/src/components/pool/PoolDetail.vue
@@ -4,7 +4,7 @@
 			<Navigation :navigationItems="navigation" :currentNavigationItem="currentNavigationItem" :navigationCallback="navigationCallback" :show="show"/>
 		</template>
 		<template v-slot:content>
-			<div v-if="navTag == 'profile'">
+			<div v-if="navTag == 'profile'" class="mt-6 grid grid-cols-4">
 				 <!-- PoolName               PoolCapacity Bar/Circle              Avg.Disk Temp per Disk in Pool? or another stat       Refresh Btn-->
 					<!-- Size: fullsize of pool storage                               Health: STATUS (checkmark if good)
 							Used: allocated storage                                       Errors: if any errors show here, if none show 'None currently'
@@ -14,31 +14,40 @@
 							# of Devices?                               # of Datasets?
 							# of Disks?
 					-->
-					<div class="flex items-center flex-wrap max-w-md px-10 bg-white rounded-2xl h-20">
-						<div class="flex items-center justify-center -m-6 overflow-visible bg-white rounded-full">
-							<svg class="w-32 h-32 transform translate-x-1 translate-y-1" aria-hidden="true">
-								<circle class="text-gray-300" stroke-width="10" stroke="currentColor" fill="transparent" r="50" cx="60" cy="60" />
-								<circle class="text-green-600" stroke-width="10" stroke-dasharray="314" :stroke-dashoffset=visualCapacity stroke-linecap="round" stroke="currentColor" fill="transparent" r="50" cx="60" cy="60" />
-							</svg>
-							<span class="absolute text-2xl text-green-700">{{props.pool.properties.capacity}}%</span>
+					<div class="col-span-2">
+						<div :id="getIdKey('visual-capacity')" class="flex items-center flex-wrap max-w-md px-10 bg-white shadow rounded-2xl h-20">
+							<div class="flex items-center justify-center -m-6 overflow-visible shadow bg-white rounded-full">
+								<svg class="w-32 h-32 transform translate-x-1 translate-y-1" aria-hidden="true">
+									<circle class="text-gray-300" stroke-width="10" stroke="currentColor" fill="transparent" r="50" cx="60" cy="60" />
+									<circle class="text-green-600" stroke-width="10" stroke-dasharray="314" :stroke-dashoffset=visualCapacity stroke-linecap="round" stroke="currentColor" fill="transparent" r="50" cx="60" cy="60" />
+								</svg>
+								<span class="absolute text-2xl text-green-700">{{props.pool.properties.capacity}}%</span>
+							</div>
+							<p class="ml-10 font-medium text-gray-600 sm:text-xl">{{ props.pool.name }}</p>
+							<span class="ml-auto text-xl font-medium text-green-600 hidden sm:block">{{ props.pool.properties.size }}</span>
 						</div>
-						<p class="ml-10 font-medium text-gray-600 sm:text-xl">{{ props.pool.name }}</p>
-						<span class="ml-auto text-xl font-medium text-green-600 hidden sm:block">{{ props.pool.properties.size }}</span>
 					</div>
-					<div class="mt-7">
-						<p>Health: {{ props.pool.status }}</p>
-						<p>Errors: None currently detected.</p>
-						<p>@ time+date</p>
+					
+					<div class="mt-2 col-span-2 col-start-3 row-start-1">
+						<p class="text-lg">Health: <span class="text-green-700">{{ props.pool.status }}</span></p>
+						<p class="text-sm">Errors: None currently detected - {{ getTimestampString }}</p>
 					</div>
 
-					<div>
-						<p>Size: {{ props.pool.properties.size }}</p>
-						<p>Used: {{ props.pool.properties.allocated }}</p>
-						<p>Free: {{ props.pool.properties.free }}</p>
+					<div class="mt-2 col-span-1 col-start-3 row-start-2">
+						<p class="text-base">Size: {{ props.pool.properties.size }}</p>
+						<p class="text-base">Used: {{ props.pool.properties.allocated }}</p>
+						<p class="text-base">Free: {{ props.pool.properties.free }}</p>
 					</div>
+					<div class="mt-2 col-span-1 col-start-4 row-start-2">
+						<p class="text-base">Devices: {{ getNumDevices }}</p>
+						<p class="text-base">Disks: {{  getNumDisks}}</p>
+						<p class="text-base">Datasets: {{ getNumDatasets }}</p>
+					</div>
+
+				
 			</div>
 
-			<div v-if="navTag == 'topology'">
+			<div v-if="navTag == 'topology'" class="mt-2 grid grid-cols-4">
 				<!-- 
 					PoolName    PoolCapacity      AvgTemp?
 
@@ -47,22 +56,35 @@
 						Disks have :
 							alias, name, model/serial, temp, status
 				 -->
-				 <div>
-					<ul :id="getIdKey('available-disk-list')" role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+				 <div class="mt-2 col-span-4">
+					<div class="grid grid-cols-4">
+						<div v-for="(vdev, vDevIdx) in props.pool.vdevs" :key="vDevIdx" class=" divide-y divide-gray-200 rounded-lg shadow">
+							<label :for="getIdKey(`vdev-${vDevIdx}`)">{{ props.pool.vdevs[vDevIdx].name }}</label>
+							<div v-for="(disk, diskIdx) in props.pool.vdevs[vDevIdx].disks" :key="diskIdx" class="col-span-1 rounded-lg ">
+								<label :for="getIdKey(`vdev-${vDevIdx}-disk-${diskIdx}`)">
+									<h3 class="truncate text-sm font-medium text-gray-900">{{ disk.name }}</h3> 
+									<p class="mt-1 truncate text-sm text-gray-500">Serial #: {{ disk.serial }}</p>
+									<p class="mt-1 truncate text-sm text-gray-500">{{ disk.sd_path }}</p>
+								</label>
+
+							</div>
+						</div>
+					</div>
+					<!-- <ul :id="getIdKey('available-disk-list')" role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
 						<li v-for="(vdev, vDevIdx) in props.pool.vdevs" :key="vDevIdx" class="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow" >
 							<li v-for="(disk, diskIdx) in props.pool.vdevs[vDevIdx].disks" :key="diskIdx" class="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow">
 								<div class="flex w-full h-full border border-gray-200 rounded dark:border-gray-700">
 									<label :for="getIdKey(`vdev-${vDevIdx}-disk-${diskIdx}`)" class="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"> 
-										<h3 class="truncate text-sm font-medium text-gray-900">{{ disk.name }}</h3>
+										<h3 class="truncate text-sm font-medium text-gray-900">{{ disk.name }}</h3> -->
 										<!-- <span class="inline-flex flex-shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">{{ disk.status }}</span> -->
-										<p class="mt-1 truncate text-sm text-gray-500">{{ disk.type }}</p>
+										<!-- <p class="mt-1 truncate text-sm text-gray-500">{{ disk.type }}</p>
 										<p class="mt-1 truncate text-sm text-gray-500">Serial #: {{ disk.serial }}</p>
 										<p class="mt-1 truncate text-sm text-gray-500">{{ disk.sd_path }}</p>
 									</label>  
 								</div>
 							</li>
 						</li>
-					</ul>
+					</ul> --> 
 				 </div>
 			
 			</div>
@@ -108,6 +130,39 @@ const visualCapacity = computed(() => {
 
 const show = ref(true);
 const navTag = ref('profile');
+
+const getTimestampString = computed(() => {
+	const currentDateTime = new Date();
+	const timestampString = currentDateTime.toLocaleString('en-US', {
+		hour: 'numeric',
+		minute: 'numeric',
+		second: 'numeric',
+		day: '2-digit',
+		month: '2-digit',
+		year: 'numeric'
+	});
+
+	return timestampString;
+});
+
+const getNumDevices = props.pool.vdevs.length;
+
+const getNumDisks = computed(() =>  {
+	let disks = 0;
+	props.pool.vdevs.forEach(vdev => {
+		vdev.disks.forEach(disk => {
+			disks++;
+		});
+	});
+	return disks;
+});
+
+const getNumDatasets = computed(() => {
+	let datasets = 0;
+	//use recursion to check root_dataset for children and those children for children etc. 
+	
+	return datasets;
+});
 
 const currentNavigationItem = computed<NavigationItem | undefined>(() => navigation.find(item => item.current));
 
