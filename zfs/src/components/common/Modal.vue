7e04d4b0519b197ff1326cf788042f3ef04764eb
JordanKeo45 <jkeough@45drives.com>
2023-07-05 13:05:05 -0300

    modal updates

--- zfs/src/components/common/Modal.vue
+++ zfs/src/components/common/Modal.vue
@@ -8,32 +8,33 @@
 			<div class="fixed inset-0 z-10 overflow-y-auto">
 				<div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
 					<TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-						<DialogPanel class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all">
-							<!-- <div class="absolute right-0 top-0 hidden pr-4 pt-4 sm:block">
+						<DialogPanel class="w-full h-full min-w-full min-h-full overflow-auto rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all">
+						<!-- <div class="absolute right-0 top-0 hidden pr-4 pt-4 sm:block">
 								<button type="button" class="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2" @click="open = false">
 									<span class="sr-only">Close</span>
 									<XMarkIcon class="h-6 w-6" aria-hidden="true" />
 								</button>
 							</div> -->
-							<div class="sm:flex sm:items-start">
-								<div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+							<div class="sm:flex sitems-center">
+								<div class="w-full mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
 									<DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900">
 										<slot name="title"/>
 									</DialogTitle>
-									<div class="mt-2">
+									<div class="mt-2 w-full">
 										<p class="text-sm text-gray-500">
 											<slot name="content"/>
 										</p>
 									</div>
 								</div>
 							</div>
-							<div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+							<div class="w-full mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
 								<slot name="footer"/>
 							</div>
 						</DialogPanel>
 					</TransitionChild>
 				</div>
 			</div>
+
 		</Dialog>
 	</TransitionRoot>
 </template>
--- zfs/src/components/pool/PoolDetail.vue
+++ zfs/src/components/pool/PoolDetail.vue
@@ -14,19 +14,27 @@
 							# of Devices?                               # of Datasets?
 							# of Disks?
 					-->
-					<div>
-						<p>{{ props.pool.name }}</p>
-					</div>
-					<div class="flex items-center flex-wrap max-w-md px-10 bg-white shadow-xl rounded-2xl h-20">
-						<div class="flex items-center justify-center -m-6 overflow-hidden bg-white rounded-full">
+					<div class="flex items-center flex-wrap max-w-md px-10 bg-white rounded-2xl h-20">
+						<div class="flex items-center justify-center -m-6 overflow-visible bg-white rounded-full">
 							<svg class="w-32 h-32 transform translate-x-1 translate-y-1" aria-hidden="true">
 								<circle class="text-gray-300" stroke-width="10" stroke="currentColor" fill="transparent" r="50" cx="60" cy="60" />
-								<circle class="text-green-600" stroke-width="10" stroke-dasharray="314" stroke-dashoffset="94" stroke-linecap="round" stroke="currentColor" fill="transparent" r="50" cx="60" cy="60" />
+								<circle class="text-green-600" stroke-width="10" stroke-dasharray="314" :stroke-dashoffset=visualCapacity stroke-linecap="round" stroke="currentColor" fill="transparent" r="50" cx="60" cy="60" />
 							</svg>
-							<span class="absolute text-2xl text-green-700">90%</span>
+							<span class="absolute text-2xl text-green-700">{{props.pool.properties.capacity}}%</span>
 						</div>
-						<p class="ml-10 font-medium text-gray-600 sm:text-xl">Storage</p>
-						<span class="ml-auto text-xl font-medium text-green-600 hidden sm:block">20GB</span>
+						<p class="ml-10 font-medium text-gray-600 sm:text-xl">{{ props.pool.name }}</p>
+						<span class="ml-auto text-xl font-medium text-green-600 hidden sm:block">{{ props.pool.properties.size }}</span>
+					</div>
+					<div class="mt-7">
+						<p>Health: {{ props.pool.status }}</p>
+						<p>Errors: None currently detected.</p>
+						<p>@ time+date</p>
+					</div>
+
+					<div>
+						<p>Size: {{ props.pool.properties.size }}</p>
+						<p>Used: {{ props.pool.properties.allocated }}</p>
+						<p>Free: {{ props.pool.properties.free }}</p>
 					</div>
 			</div>
 
@@ -39,6 +47,23 @@
 						Disks have :
 							alias, name, model/serial, temp, status
 				 -->
+				 <div>
+					<ul :id="getIdKey('available-disk-list')" role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+						<li v-for="(vdev, vDevIdx) in props.pool.vdevs" :key="vDevIdx" class="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow" >
+							<li v-for="(disk, diskIdx) in props.pool.vdevs[vDevIdx].disks" :key="diskIdx" class="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow">
+								<div class="flex w-full h-full border border-gray-200 rounded dark:border-gray-700">
+									<label :for="getIdKey(`vdev-${vDevIdx}-disk-${diskIdx}`)" class="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"> 
+										<h3 class="truncate text-sm font-medium text-gray-900">{{ disk.name }}</h3>
+										<!-- <span class="inline-flex flex-shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">{{ disk.status }}</span> -->
+										<p class="mt-1 truncate text-sm text-gray-500">{{ disk.type }}</p>
+										<p class="mt-1 truncate text-sm text-gray-500">Serial #: {{ disk.serial }}</p>
+										<p class="mt-1 truncate text-sm text-gray-500">{{ disk.sd_path }}</p>
+									</label>  
+								</div>
+							</li>
+						</li>
+					</ul>
+				 </div>
 			
 			</div>
 			
@@ -77,6 +102,10 @@ interface PoolDetailsProps {
 
 const props = defineProps<PoolDetailsProps>();
 
+const visualCapacity = computed(() => {
+	return 314 - (props.pool.properties.capacity * 100) / 314;
+})
+
 const show = ref(true);
 const navTag = ref('profile');
 
@@ -92,4 +121,6 @@ const navigation = reactive<NavigationItem[]>([
 	{ name: 'Snapshots', tag: 'snapshots', current: computed(() => navTag.value == 'snapshots') as unknown as boolean, show: true, },
 	{ name: 'Settings', tag: 'settings', current: computed(() => navTag.value == 'settings') as unknown as boolean, show: true, },
 ].filter(item => item.show));
+
+const getIdKey = (name: string) => `${name}`;
 </script>
