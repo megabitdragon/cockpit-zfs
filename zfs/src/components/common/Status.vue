9e745521e9c3de6e3f6f9aa39ba32a80ca50c886
JordanKeo45 <jkeough@45drives.com>
2023-09-28 16:27:14 -0300

    more development of status component

--- zfs/src/components/common/Status.vue
+++ zfs/src/components/common/Status.vue
@@ -1,55 +1,46 @@
 <template>
     <div>
         <div class="grid grid-cols-4 gap-1 justify-items-center">
-            <span v-if="!paused" class="text-muted col-span-4">
+
+            <span class="col-span-4" :class="stateMessageClass()">
                 {{ stateMessage }}
             </span>
-       
-            <span class="text-muted col-span-4">
-                {{ amountProcessed }} of {{ amountTotal }} processed. <br/>
-                <!-- {{ amountRemaining }} remaining. <br/> -->
-                <!-- bytes processed: {{ scanObject.bytes_processed }} <br/>
-                bytes issued: {{ scanObject.bytes_issued }} <br/>
-                bytes to process: {{ scanObject.bytes_to_process }} <br/> -->
-            </span>
-            <span class="text-muted col-span-4">
-                Completes in {{ timeRemaining }}
-            </span>
-            <span v-if="paused && !operationRunning && !operationCompleted" class="text-muted col-span-4">
-                {{ scanFunction }} paused at {{ scanObject.pause }}
-            </span>
 
+            <span v-if="isScanning && !isPaused" class="col-span-4">
+                Completes in {{ timeRemaining }}.
+            </span>
+       
             <div class="col-span-4 min-w-max w-full bg-well rounded-full relative flex h-6 min-h-min max-h-max overflow-hidden">
-                <div class="bg-green-600 h-6 min-h-min max-h-max" :style="{ width: `${parseFloat(scanPercentage.toFixed(2))}%` }">
+                <div :class="progressBarClass()" class="h-6 min-h-min max-h-max" :style="{ width: `${parseFloat(scanPercentage.toFixed(2))}%` }">
                     <div class="absolute inset-0 flex items-center justify-center text-s font-medium text-default text-center p-0.5 leading-none">
                         {{ parseFloat(scanPercentage.toFixed(2)) }}%
                     </div>
                 </div>
             </div>
 
+            <span class="text-muted col-span-4">
+                {{ amountProcessed }} of {{ amountTotal }} processed. <br/>
+            </span>
+
         </div>
-        <!-- <span>{{ scanObject }}</span> -->
     </div>
 </template>
 <script setup lang="ts">
 import { ref, inject, Ref, computed, provide, watch, watchEffect } from "vue";
 import { convertBytesToSize, convertSecondsToString } from "../../composables/helpers";
 import { loadScanObject } from '../../composables/loadData';
-import CircleProgress from "./CircleProgress.vue";
 
 interface StatusProps {
     poolName: string;
 }
 
 const props = defineProps<StatusProps>();
-const operationRunning = ref(false);
-const operationCompleted = ref(false);
 const paused = ref(false);
 const scanObject = inject<Ref<PoolScanObject>>('scan-object')!;
 
 //method to repeatedly check scan object
 async function continuousScanCheck() {
-    if (!operationCompleted) {
+    if (!isFinished) {
         setInterval(async () => {
             await loadScanObject(scanObject, props.poolName);
             console.log('continuous scan object:', scanObject.value);
@@ -62,11 +53,8 @@ async function scanOnLoad() {
     await loadScanObject(scanObject, props.poolName);
     console.log('scan@load object:', scanObject.value);
     if (isScanning.value) {
-        operationRunning.value = true;
         continuousScanCheck();
-    } else {
-        operationRunning.value = false;
-    }
+    } 
 }
 
 //method to check scan when user input is given (click on resilver, scrub, or refresh)
@@ -74,17 +62,36 @@ async function scanOnInput() {
     await loadScanObject(scanObject, props.poolName);
     console.log('scan@input object:', scanObject.value);
     if (isScanning.value) {
-        operationRunning.value = true;
         continuousScanCheck();
-    } else {
-        operationRunning.value = false;
     }
-}
+ }
 
 scanOnLoad();
 
 const isScanning = computed(() => {
-    return scanObject.value.state === 'SCANNING';
+    if (scanObject.value.state === 'SCANNING') {
+        return true;
+    } else {
+        return false;
+    }
+});
+
+
+const isFinished = computed(() => {
+    if (scanObject.value.state === 'FINISHED') {
+        return true;
+    } else {
+        return false;
+    }
+});
+
+
+const isCanceled = computed(() => {
+    if (scanObject.value.state === 'CANCELED') {
+        return true;
+    } else {
+        return false;
+    }
 });
 
 const isPaused = computed(() => {
@@ -107,17 +114,21 @@ const scanFunction = computed(() => {
 
 //scan state: NONE | SCANNING | FINISHED | CANCELED
 const stateMessage = computed(() => {
-    switch (scanObject.value.state) {
-        case 'SCANNING':
-            return `${scanObject.value.function} in progress at ${scanObject.value.start_time}`;
-        case 'FINISHED':
-            return `${scanObject.value.function} finished at ${scanObject.value.end_time}`;
-        case 'CANCELED':
-            return `${scanObject.value.function} canceled at ${scanObject.value.end_time}`;
-        case 'NONE':
-            return 'None';
-        default:
-            return '';
+    if (scanObject.value.pause !== 'None') {
+       return `${scanObject.value.function} paused at ${scanObject.value.pause}`;
+    } else {
+        switch (scanObject.value.state) {
+            case 'SCANNING':
+                return `${scanObject.value.function} started at ${scanObject.value.start_time}`;
+            case 'FINISHED':
+                return `${scanObject.value.function} finished at ${scanObject.value.end_time}`;
+            case 'CANCELED':
+                return `${scanObject.value.function} canceled at ${scanObject.value.end_time}`;
+            case 'NONE':
+                return 'None';
+            default:
+                return '';
+        }
     }
 });
 
@@ -137,10 +148,52 @@ const timeRemaining = computed(() => {
     return convertSecondsToString(scanObject.value.total_secs_left);
 });
 
+function stateMessageClass() {
+    if (scanObject.value.pause === 'None') {
+        switch (scanObject.value.state) {
+            case 'SCANNING':
+                return 'text-default';
+            case 'FINISHED':
+                return 'text-success'; 
+            case 'CANCELED':
+                return 'text-danger'; 
+            case 'NONE':
+                return 'text-muted';
+            default:
+                return 'text-default'; 
+        } 
+    } else {
+        return 'text-orange-600'
+    }
+}
+
+function progressBarClass() {
+    if (scanObject.value.pause === 'None') {
+        switch (scanObject.value.state) {
+            case 'SCANNING':
+                return 'bg-green-600';
+            case 'FINISHED':
+                return 'bg-green-600'; 
+            case 'CANCELED':
+                return 'bg-danger'; 
+            case 'NONE':
+                return 'text-muted';
+            default:
+                return 'text-default'; 
+        }
+    } else {
+        return 'bg-orange-600';
+    }
+   
+}
+
+
+
+
 watch(scanObject, (newVal, oldVal) => {
-    operationRunning.value = isScanning.value;
-    operationCompleted.value = newVal.state === 'FINISHED' || newVal.state === 'CANCELED';
-    paused.value = isPaused.value;
+    // operationRunning.value = isScanning.value;
+    // operationCompleted.value = newVal.state === 'FINISHED' || newVal.state === 'CANCELED';
+    // paused.value = isPaused.value;
 
 });
 
