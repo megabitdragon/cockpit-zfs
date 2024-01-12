6535b4123c913200495d5c97bffba11f4dcd0613
JordanKeo45 <jkeough@45drives.com>
2024-01-12 11:26:06 -0400

    fixes margin of menu button dropdown

--- zfs/src/components/file-systems/FileSystemList.vue
+++ zfs/src/components/file-systems/FileSystemList.vue
@@ -48,7 +48,7 @@
 															class="-mt-2 h-10 w-10 text-default transition-all duration-200 transform" :class="{ 'rotate-90': !open, 'rotate-180': open, }"
 														/>
 													</div>
-													<div class="py-1 mt-1 col-span-1 flex flex-row justify-start" :title="fileSystem.name">{{ fileSystem.name.length > 20 ? fileSystem.name.slice(0, 20) + '...' : fileSystem.name }}</div>
+													<div class="py-1 mt-1 col-span-1 flex flex-row justify-start whitespace-nowrap" :title="fileSystem.name">{{ fileSystem.name.length > 18 ? fileSystem.name.slice(0, 18) + '...' : fileSystem.name }}</div>
 													<div class="py-1 mt-1 col-span-1">{{ convertBytesToSize(fileSystem.properties.available) }}</div>
 													<div class="py-1 mt-1 col-span-1">{{ fileSystem.properties.usedByDataset }}</div>
 													<div class="py-1 mt-1 col-span-1">{{ fileSystem.properties.usedbyRefreservation }}</div>
@@ -68,7 +68,7 @@
 															</div>
 
 															<transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-																<MenuItems class="absolute right-0 z-10 mt-1 w-56 origin-top-right rounded-md bg-accent shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+																<MenuItems class="absolute right-0 z-10 w-56 origin-top-right rounded-md bg-accent shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
 																	<div class="py-1">
 																		<MenuItem as="div" v-slot="{ active }">
 																			<a href="#" @click="loadFileSystemConfig(fileSystems[fsIdx])" :class="[active ? 'bg-default text-default' : 'text-muted', 'block px-4 py-2 text-sm']">Configure File System</a>
