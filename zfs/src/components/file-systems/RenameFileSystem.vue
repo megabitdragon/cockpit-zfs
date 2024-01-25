6e3b54f1ad3591ef198364cfa64eb88b5bdbb0de
JordanKeo45 <jkeough@45drives.com>
2024-01-25 11:24:38 -0400

    adjusts spacing of elements in clone/rename snap and rename filesystem

--- zfs/src/components/file-systems/RenameFileSystem.vue
+++ zfs/src/components/file-systems/RenameFileSystem.vue
@@ -1,5 +1,5 @@
 <template>
-    <Modal :isOpen="showRenameModal" @close="showRenameModal = false" :marginTop="'mt-28'" :width="'w-3/5'" :minWidth="'min-w-3/5'">
+    <Modal :isOpen="showRenameModal" @close="showRenameModal = false" :marginTop="'mt-28'" :width="'w-4/12'" :minWidth="'min-w-4/12'">
         <template v-slot:title>
             <legend class="flex justify-center">Rename File System</legend>
         </template>
@@ -71,11 +71,11 @@
                     <p class="text-danger" v-if="nameFeedback">{{ nameFeedback }}</p>
                     <p class="text-danger" v-if="parentFeedback">{{ parentFeedback }}</p>
                 </div>
-                <div class="button-group-row mt-2 justify-between">
-                    <button @click="showRenameModal = false" :id="getIdKey('confirm-rename-no')" name="rename-button-no" class="mt-1 btn btn-secondary object-left justify-start h-fit">Cancel</button>
+                <div class="button-group-row justify-between">
+                    <button @click="showRenameModal = false" :id="getIdKey('confirm-rename-no')" name="rename-button-no" class="btn btn-secondary object-left justify-start h-fit">Cancel</button>
                 
                     <div class="button-group-row">
-                        <button v-if="!renaming" id="rename-filesystem-btn" class="btn btn-primary object-right justify-end mr-4 h-fit w-full" @click="renameBtn()">Rename</button>
+                        <button v-if="!renaming" id="rename-filesystem-btn" class="btn btn-primary object-right justify-end h-fit w-full" @click="renameBtn()">Rename</button>
                         <button disabled v-if="renaming" id="finish" type="button" class="btn btn-primary object-right justify-end">
                             <svg aria-hidden="true" role="status" class="inline w-4 h-4 mr-3 text-gray-200 animate-spin text-default" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
