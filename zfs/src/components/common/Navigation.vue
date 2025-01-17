82c355d0cf8327535692207856f5a894ef6e87bf
JordanKeo45 <jkeough@45drives.com>
2025-01-17 11:22:25 -0400

    removes non-functioning mobile navigation

--- zfs/src/components/common/Navigation.vue
+++ zfs/src/components/common/Navigation.vue
@@ -1,16 +1,6 @@
 <template>
 	<div class="bg-default">
-		<div class="sm:hidden">
-			<label for="tabs" class="sr-only">Select a tab</label>
-			<!-- Use an "onChange" listener to redirect the user to the selected tab URL. -->
-			<select id="tabs" name="tabs" class="block w-full rounded-md border-default py-2 pl-3 pr-10 text-base focus:border-fefault focus:outline-none focus:ring-slate-500 sm:text-sm">
-				<option v-for="item in props.navigationItems" :key="item.name" @change="navigationCallback(item)" :selected="item.current">{{ item.name }}</option>
-			</select>
-		</div>
-		<!--^^^ Mobile or very small screen --- DOES NOT WORK YET (onChange wont work for some reason) ^^^-->
-
-		<!--vvv Desktop/Regular version vvv-->
-		<div class="hidden sm:block">
+		<div>
 			<div class="border-b border-default">
 				<nav class="-mb-px flex justify-center" aria-label="Tabs">
 					<a v-for="item in props.navigationItems" :key="item.name"
